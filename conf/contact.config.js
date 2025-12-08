25eb400603c5d2f22bda243b111f96b4d8e78962
Stickman.life <Stickman.life@outlook.com>
2025-12-09 00:19:03 +0800

    change theme

--- conf/contact.config.js
+++ conf/contact.config.js
@@ -8,8 +8,6 @@ module.exports = {
       btoa(
         unescape(encodeURIComponent(process.env.NEXT_PUBLIC_CONTACT_EMAIL))
       )) ||
-    '', // 邮箱地址 例如mail@tangly1024.com
+    'stickman.life@outlook.com', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || 'https://github.com/JokerLJZ', // 你的github个人主页 例如 https://github.com/tangly1024
-  CONTACT_LINKEDIN: process.env.NEXT_PUBLIC_CONTACT_LINKEDIN || '', // 你的linkedIn 首页
-  CONTACT_BILIBILI: process.env.NEXT_PUBLIC_CONTACT_BILIBILI || '', // B站主页
 }
